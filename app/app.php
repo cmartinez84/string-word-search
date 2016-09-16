e2b483f3251fbc17dbabfc7bb76043b20207d04b
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-16 10:44:12 -0700

    refactor class to hold user input for display purposese with results

--- app/app.php
+++ app/app.php
@@ -8,17 +8,19 @@
     //     $_SESSION['list_of_albums'] = array();
     // }
     $app = new Silex\Application();
-    
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('home.html.twig');
     });
+
     $app->post("/result", function() use ($app) {
-        $newDayOfWeek = new DayOfWeek;
-        $day = $newDayOfWeek->getDayOfWeek($_POST['month'], $_POST['date'], $_POST['year']);
-        return $app['twig']->render('home.html.twig', array('day' => $day));
+        $newRepeatCounter = new RepeatCounter;
+        $newRepeatCounter->CountRepeats($_POST['input_phrase'], $_POST['input_word']);
+        return $app['twig']->render('home.html.twig', array('result' => $newRepeatCounter));
     });
     return $app;
 ?>
--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,19 +1,42 @@
 <?php
     class RepeatCounter
     {
-        function CountRepeats($phrase, $input_word){
+        private $phrase;
+        private $word;
+        private $frequency;
+        private $plural = "";
+
+        function CountRepeats($phrase, $word){
+            $this->phrase = $phrase;
+            $this->word = $word;
             $phrase = strtr($phrase,".,:;?!-\'\"$%@#^*({})][]\\/<> +=_", "                                ");
             $phrase = strtolower($phrase);
-            $input_word = strtolower($input_word);
+            $word = strtolower($word);
             $result = 0;
             $phraseArray = explode(" ", $phrase);
             $result = 0;
-            foreach($phraseArray as $word){
-                if ($word == $input_word){
+            foreach($phraseArray as $phraseWord){
+                if ($word == $phraseWord){
                     $result += 1;
                 }
             }
+            $this->frequency = $result;
+            if($result !=1){
+                $this->plural = "s";
+            }
             return $result;
+        }
+        function getPhrase(){
+            return $this->phrase;
+        }
+        function getWord(){
+            return $this->word;
+        }
+        function getfrequency(){
+            return $this->frequency;
+        }
+        function getplural(){
+            return $this->plural;
         }
     }
  ?>
