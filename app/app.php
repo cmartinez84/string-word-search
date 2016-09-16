7be802dfa9cf599fe2cbb16a077f12886e74d49e
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-16 11:51:57 -0700

    will only display the title of the passage for phrase inputs other than user input, ex Hamlets Soliliquy

--- app/app.php
+++ app/app.php
@@ -1,6 +1,8 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
+    require_once __DIR__."/../src/Data.php";
+
 
     date_default_timezone_set('America/Los_Angeles');
     // session_start();
@@ -16,10 +18,11 @@
     $app->get("/", function() use ($app) {
         return $app['twig']->render('home.html.twig');
     });
-
     $app->post("/result", function() use ($app) {
         $newRepeatCounter = new RepeatCounter;
-        $newRepeatCounter->CountRepeats($_POST['input_phrase'], $_POST['input_word']);
+        $newData = new Data;
+        $input = $newData->getData($_POST['input_phrase']);
+        $newRepeatCounter->CountRepeats($input, $_POST['input_word']);
         return $app['twig']->render('home.html.twig', array('result' => $newRepeatCounter));
     });
     return $app;
