b33f59b2a05affdb55c337fdab2c6607ed567f0c
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-16 11:14:34 -0700

    add data file to use as string input

--- web/index.php
+++ web/index.php
@@ -7,6 +7,8 @@
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <meta charset="utf-8">
+        <link rel="stylesheet" href="css/mystyles.css" type="text/css">
+        <meta charset="utf-8">
         <title></title>
     </head>
     <body>
